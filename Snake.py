bb54863ce52de35eeec45e8b0b274c3ed917c67d
Christian Hey <c.hey@sbs-herzogenaurach.de>
2024-03-14 10:41:33 +0100

    Solved task 1

--- Snake.py
+++ Snake.py
@@ -1,5 +1,7 @@
+import random
+
 from PyQt6.QtCore import Qt, QRect
-from PyQt6.QtGui import QPaintEvent, QPainter, QKeyEvent
+from PyQt6.QtGui import QPaintEvent, QPainter, QKeyEvent, QColor, QBrush
 from PyQt6.QtWidgets import QLabel
 
 
@@ -8,10 +10,26 @@ class Snake(QLabel):
         super(Snake, self).__init__(parent)
 
         self.__delta = 10
-        self.setFixedSize(30 * self.__delta, 30 * self.__delta)
+        self.__number_x = 30
+        self.__number_y = 25
+
+        self.__w = self.__number_x * self.__delta
+        self.__h = self.__number_y * self.__delta
+        self.__field = QRect(0, 0, self.__w, self.__h)
+
+        self.setFixedSize(self.__field.size())
+
+        self.__brush_black = QBrush(QColor("black"))
+        self.__brush_yellow = QBrush(QColor("yellow"))
+        self.__brush_red = QBrush(QColor("red"))
 
         self.__list_of_rects = list()
         self.__list_of_rects.append(QRect(15 * self.__delta, 15 * self.__delta, self.__delta, self.__delta))
+
+        random.seed("debug")
+        #random.seed()
+        self.__loot_x = random.randrange(0, self.__number_x) * self.__delta
+        self.__loot_y = random.randrange(0, self.__number_y) * self.__delta
 
         self.activateWindow()
         self.setFocusPolicy(Qt.FocusPolicy.StrongFocus)
@@ -19,14 +37,18 @@ class Snake(QLabel):
     def paintEvent(self, a0: QPaintEvent) -> None:
         painter = QPainter(self)
 
-        # Zeichnen Sie den Hintergrund in der Farbe Schwarz.
+        # paint background
+        painter.setBrush(self.__brush_black)
+        painter.drawRect(self.__field)
 
-        painter.drawRects(self.__list_of_rects)
-        # Füllen Sie die Rechtecke der Schlage in der Farbe gelb aus.
+        # paint snake
+        for rect in self.__list_of_rects:
+            painter.drawRect(rect)
+            painter.fillRect(rect, self.__brush_yellow)
 
-        # Zeichen Sie einen roten Kreis, welche unsere Schlange später isst.
-        # Der Kreis erhält einmalig zufällige Koordinaten. Die Koordinaten werden bis zum
-        # verschlingen der Beute gespeichert.
+        # paint loot
+        painter.setBrush(self.__brush_red)
+        painter.drawEllipse(self.__loot_x, self.__loot_y, self.__delta, self.__delta)
 
     def keyReleaseEvent(self, ev: QKeyEvent) -> None:
         super(Snake, self).keyReleaseEvent(ev)
